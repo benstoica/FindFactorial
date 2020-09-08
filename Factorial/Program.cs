cb4f5e1d22fea0cff9fa20a664701e3b41d0e078
Ben Stoica <bennystoica@gmail.com>
2020-09-08 19:51:13 -0400

    initial commit - factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, number, factorial;
+            Console.WriteLine("Enter a number to see the factorial: ");
+            number = int.Parse(Console.ReadLine());
+            factorial = number;
+            for (i = number - 1; i >= 1; i--)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine($"{number} factorial is: {factorial}");
+            Console.ReadKey();
+        }
+    }
+}
